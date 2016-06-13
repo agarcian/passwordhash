b498b9e5d8eb0642e8726b4e79758439f83539c6
Andres Garcia <Andres.Garcia@florencehc.com>
2016-06-13 15:08:32 -0400

    Refactored to implement tests with mocha and chai.

--- test/test.js
+++ test/test.js
@@ -1,89 +1,68 @@
 'use strict';
 
-var Code = require('code');   // assertion library
-var Lab = require('lab');
-var lab = exports.lab = Lab.script();
+var expect = require("chai").expect;
 
 var passwordhash = require('../index');
 
-// Notes on lab.
-// return done() to indicate success.
-// return done(err) to indicate failure.
-// if no done is returned, it indicates that the test is in development and it will timeout (2s) and fail.
-// If no callback function is provided, the test is considered a TO DO reminder and will be skipped.
+describe('password hash', function() {
 
-
-lab.experiment('password hash', function() {
-
-    lab.test('returns true when validating a password hash', function(done) {
+    it('should return true when validating a password hash', function(done) {
 
         passwordhash.createHash('my password', function(err, hash) {
 
+            expect(err).to.be.null;
             passwordhash.validateHash('my password', hash, function(err, success) {
 
-                Code.expect(err).to.be.null();
-                Code.expect(success).to.be.true();
-                done();   // do done(err) if failed.
-
+                expect(err).to.be.null;
+                expect(success).to.be.true;
+                done();
             });
-
         });
-
     });
 
-    lab.test('return false when validating an incorrect password and a hash', function(done) {
+    it('should return false when validating an incorrect password and a hash', function(done) {
 
         passwordhash.createHash('my password', function(err, hash) {
 
+            expect(err).to.be.null;
             passwordhash.validateHash('my BAD password', hash, function(err, success) {
 
-                Code.expect(err).to.be.null();  // No error is expected, simply a non-match.
-                Code.expect(success).to.be.false();
+                expect(err).to.be.null;  // No error is expected, simply a non-match.
+                expect(success).to.be.false;
                 done();
-
             });
-
         });
-
     });
 
-    lab.test('return error when sending an invalid hash to validate', function(done) {
+    it('should return error when sending an invalid hash to validate', function(done) {
 
         passwordhash.validateHash('my BAD password', "invalid hash", function(err, success) {
 
-            Code.expect(err).to.be.not.null();  // No error is expected, simply a non-match.
-            Code.expect(success).to.be.false();
+            expect(err).to.be.not.null;  // No error is expected, simply a non-match.
+            expect(success).to.be.false;
             done();
-
         });
-
     });
 
-    lab.test('Validates a hash created with other system (.net) with a known password.', function(done) {
+    it('should validate a hash created with other system (.net) with a known password.', function(done) {
 
         var existingHash = "sha1:20000:p5GwEABDCx/HwW9p2gMXoyUAMXkWNPU4:dRUwYNvT0iapWk3l+OaiFPzb6z/ii20r";
         passwordhash.validateHash('Florence123', existingHash, function(err, success) {
 
-            Code.expect(err).to.be.null();  // No error is expected, simply a non-match.
-            Code.expect(success).to.be.true();
+            expect(err).to.be.null;  // No error is expected, simply a non-match.
+            expect(success).to.be.true;
             done();
-
         });
-
     });
 
-    lab.test('Validates a hash created with other system (.net) with an invalid password.', function(done) {
+    it('should validate a hash created with other system (.net) with an invalid password.', function(done) {
 
         var existingHash = "sha1:20000:p5GwEABDCx/HwW9p2gMXoyUAMXkWNPU4:dRUwYNvT0iapWk3l+OaiFPzb6z/ii20r";
         passwordhash.validateHash('BadPassword', existingHash, function(err, success) {
 
-            Code.expect(err).to.be.null();  // No error is expected, simply a non-match.
-            Code.expect(success).to.be.false();
+            expect(err).to.be.null;  // No error is expected, simply a non-match.
+            expect(success).to.be.false;
             done();
-
         });
-
     });
-
-
 });
